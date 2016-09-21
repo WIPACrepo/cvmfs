e9a20de8724ded6223003174f43bae26adba49be
Benedikt Riedel <benedikt.riedel@gmail.com>
2016-09-21 16:02:02 -0500

    Adding symlinks for python3.

--- builders/tools/python.py
+++ builders/tools/python.py
@@ -26,7 +26,18 @@ def install(dir_name,version=None):
                 raise Exception('python failed to make')
             if subprocess.call(['make','install'],cwd=python_dir):
                 raise Exception('python failed to install')
-            
+            # Python 3 specific symlinks
+            # Assumes no python2 version is installed
+            if int(version.split(".")[0]) == 3:
+                version_short = ".".join(str(version).split(".")[0:2])
+                os.symlink(os.path.join(dir_name,'bin','python3'),
+                           os.path.join(dir_name,'bin','python'))
+                os.symlink(os.path.join(dir_name, 'bin', 'python3-config'),
+                           os.path.join(dir_name, 'bin', 'python-config'))
+                os.symlink(os.path.join(dir_name, 'lib', 'pkgconfig', 'python3.pc'),
+                           os.path.join(dir_name, 'lib', 'pkgconfig', 'python.pc'))
+                os.symlink(os.path.join(dir_name, 'include', 'python%sm' % version_short),
+                           os.path.join(dir_name, 'include', 'python%s' % version_short))
             # check for modules
             for m in ('sqlite3','zlib','bz2','_ssl','_curses','readline'):
                 if subprocess.call([os.path.join(dir_name,'bin','python'),
