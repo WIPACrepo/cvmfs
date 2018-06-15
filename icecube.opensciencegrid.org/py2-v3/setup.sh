cfa27f066e038a0215d2485a1ed7bd1f7d62b83c
David Schultz <davids24@gmail.com>
2018-06-15 14:29:46 -0500

    tabs vs spaces

--- icecube.opensciencegrid.org/py2-v3/setup.sh
+++ icecube.opensciencegrid.org/py2-v3/setup.sh
@@ -11,7 +11,7 @@
 # the first argument.  The third argument is ":" for paths, " " otherwise
 listContains() {
     case "$3$1$3" in
-	*"$3$2$3"*) return 0;;
+        *"$3$2$3"*) return 0;;
     esac
     return 1
 }
@@ -24,32 +24,32 @@ modifyList() {
     list="$1"
     new="$2"
     if [ -z ${3} ]; then
-	sep=" "
+        sep=" "
     else
-	sep=${3}
+        sep=${3}
     fi
     key="$4"
 
     #echo "VAR \"$list\" NEW \"$new\" SEP \"$sep\" KEY \"$key\"" >&2
 
     if listContains "$list" "$new" "$sep"; then
-	echo $list
-	#echo $list   "# one" >&2
-	return 0
+        echo $list
+        #echo $list   "# one" >&2
+        return 0
     fi
 
     if [ -z "$list" ]; then
-	echo $new
-	#echo $new   "# two" >&2
-	return 0
+        echo $new
+        #echo $new   "# two" >&2
+        return 0
     fi
 
     if [ "$key" = "before" ]; then
-	echo "$new$sep$list"
-	#echo "$new$sep$list"   "# before" >&2
+        echo "$new$sep$list"
+        #echo "$new$sep$list"   "# before" >&2
     else
-	echo "$list$sep$new"
-	#echo "$list$sep$new"   "# after" >&2
+        echo "$list$sep$new"
+        #echo "$list$sep$new"   "# after" >&2
     fi
     return 0
 }
@@ -68,27 +68,27 @@ prependPath() {
 findJava() {
     echo "OS_ARCH \"$OS_ARCH\"" >&2
     case $OS_ARCH in
-	RHEL_6_x86_64)
-	    LIST="/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.33.x86_64
+        RHEL_6_x86_64)
+            LIST="/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.33.x86_64
                   /usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64
                   /usr/lib/java"
-	    ;;
-	RHEL_5_i686) LIST=/usr/java/jdk1.5.0_12   ;;
-	RHEL_5_x86_64) LIST=/usr/java/default     ;;
-	RHEL_4_i686) LIST=/usr/java/j2sdk1.4.2_14 ;;
-	RHEL_4_x86_64) LIST=/usr/java/j2sdk1.4.2  ;;
-	Ubuntu_16.04_x86_64)
-	    LIST=/usr/lib/jvm/java-8-openjdk-amd64/jre
-	    ;;
-	*) LIST=                                  ;;
+            ;;
+        RHEL_5_i686) LIST=/usr/java/jdk1.5.0_12   ;;
+        RHEL_5_x86_64) LIST=/usr/java/default     ;;
+        RHEL_4_i686) LIST=/usr/java/j2sdk1.4.2_14 ;;
+        RHEL_4_x86_64) LIST=/usr/java/j2sdk1.4.2  ;;
+        Ubuntu_16.04_x86_64)
+            LIST=/usr/lib/jvm/java-8-openjdk-amd64/jre
+            ;;
+        *) LIST=                                  ;;
     esac
 
     # look through the list for a valid directory
     for dir in $LIST; do
-	if [ -d ${dir} ]; then
-	    echo $dir
-	    return 0
-	fi
+        if [ -d ${dir} ]; then
+            echo $dir
+            return 0
+        fi
     done
 }
 
@@ -109,8 +109,8 @@ case "$SROOTBASE" in
     *"py2-v"*) _version=2;;
     *"py3-v"*) _version=3;;
     *)
-	echo "$0: Can't set version for path \"$0\"" >&2
-	exit 1
+        echo "$0: Can't set version for path \"$0\"" >&2
+        exit 1
 esac
 
 # set operating system variables DISTRIB, VERSION, ARCH, and OS_ARCH
@@ -118,7 +118,7 @@ esac
 
 # VARS is the list of variables to be exported to the user's environment
 #
-VARS="SROOTBASE OS_ARCH"
+VARS="SROOTBASE SROOT OS_ARCH"
 
 # set the architecture-specific root directory
 SROOT=$SROOTBASE/$OS_ARCH
@@ -126,15 +126,15 @@ SROOT=$SROOTBASE/$OS_ARCH
 # if the binary or library directory doesn't exist, surrender to nihilism
 sroot_bin=$SROOT/bin
 sroot_lib=$SROOT/lib
-if [ ! -d ${sroot_bin} ]; then
-    echo "$0: Binary directory \"$sroot_bin\" doesn't exist" >&2
-    exit 1
-else
-    if [ ! -d ${sroot_lib} ]; then
-	echo "$0: Library directory \"$sroot_lib\" doesn't exist" >&2
-	exit 1
-    fi
-fi
+#if [ ! -d ${sroot_bin} ]; then
+#    echo "$0: Binary directory \"$sroot_bin\" doesn't exist" >&2
+#    exit 1
+#else
+#    if [ ! -d ${sroot_lib} ]; then
+#        echo "$0: Library directory \"$sroot_lib\" doesn't exist" >&2
+#        exit 1
+#    fi
+#fi
 
 # prepend the standard binary directory to the user's PATH
 PATH=$(prependPath "$PATH" "$sroot_bin")
@@ -160,15 +160,15 @@ if [ -d "$sroot_ports" ]; then
     # add I3_PORTS library to LD_LIBRARY_PATH
     i3p_plib=$sroot_ports/lib
     if [ -d ${i3p_plib} ]; then
-	LD_LIBRARY_PATH=$(prependPath "$LD_LIBRARY_PATH" "$i3p_plib")
-	VARS=$(modifyList "$VARS" "LD_LIBRARY_PATH")
+        LD_LIBRARY_PATH=$(prependPath "$LD_LIBRARY_PATH" "$i3p_plib")
+        VARS=$(modifyList "$VARS" "LD_LIBRARY_PATH")
 
-	# add I3_PORTS Python library to PYTHONPATH
-	i3p_ppkg=$i3p_plib/python2.7/site-packages
-	if [ -d ${i3p_ppkg} ]; then
-	    PYTHONPATH=$(prependPath "$PYTHONPATH" "$i3p_ppkg")
-	    VARS=$(modifyList "$VARS" "PYTHONPATH")
-	fi
+        # add I3_PORTS Python library to PYTHONPATH
+        i3p_ppkg=$i3p_plib/python2.7/site-packages
+        if [ -d ${i3p_ppkg} ]; then
+            PYTHONPATH=$(prependPath "$PYTHONPATH" "$i3p_ppkg")
+            VARS=$(modifyList "$VARS" "PYTHONPATH")
+        fi
     fi
 fi
 
@@ -181,8 +181,8 @@ if [ -d "$sroot_data" ]; then
     # initialize I3_TESTDATA variable
     sroot_tstdata=$sroot_data/i3-test-data
     if [ -d ${sroot_tstdata} ]; then
-	I3_TESTDATA=${sroot_tstdata}
-	VARS=$(modifyList "$VARS" "I3_TESTDATA")
+        I3_TESTDATA=${sroot_tstdata}
+        VARS=$(modifyList "$VARS" "I3_TESTDATA")
     fi
 fi
 
@@ -198,7 +198,7 @@ tmp_path=
 for subdir in perl perl5 perl5/site_perl; do
     tmp_dir="$SROOT/lib/$subdir"
     if [ -d ${tmp_dir} ]; then
-	tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
+        tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
     fi
 done
 if [ ! -z ${tmp_path} ]; then
@@ -211,7 +211,7 @@ tmp_path=
 for subdir in man share/man; do
     tmp_dir="$SROOT/$subdir"
     if [ -d ${tmp_dir} ]; then
-	tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
+        tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
     fi
 done
 if [ ! -z ${tmp_path} ]; then
@@ -245,14 +245,14 @@ if ([ -z ${tmp_home} ] || [ ! -f ${tmp_home}/bin/java ]); then
         for subdir in lib jre/lib; do
             tmp_dir=${tmp_home}/${subdir}
             if [ -d ${tmp_dir} ]; then
-		tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
+                tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
             fi
         done
         for archdir in amd64 i386; do
             for subdir in $archdir $archdir/server; do
                 tmp_dir=${tmp_home}/jre/lib/$subdir
                 if [ -d ${tmp_dir} ]; then
-		    tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
+                    tmp_path=$(appendPath "$tmp_path" "$tmp_dir")
                 fi
             done
         done
@@ -261,7 +261,7 @@ if ([ -z ${tmp_home} ] || [ ! -f ${tmp_home}/bin/java ]); then
             JAVA_HOME=${tmp_home}
             VARS=$(modifyList "$VARS" "JAVA_HOME")
 
-	    LD_LIBRARY_PATH=$(prependPath "$LD_LIBRARY_PATH" "$tmp_path")
+            LD_LIBRARY_PATH=$(prependPath "$LD_LIBRARY_PATH" "$tmp_path")
             VARS=$(modifyList "$VARS" "JAVA_HOME")
         fi
     fi
@@ -280,11 +280,11 @@ fi
 if [ ! -z "$X509_USER_PROXY" ]; then
     RET=`basename "$X509_USER_PROXY"`
     if [ "$RET" = "$X509_USER_PROXY" ]; then
-	# if X509_USER_PROXY is just a filename, qualify it
-	X509_USER_PROXY=$(pwd)/$X509_USER_PROXY
+        # if X509_USER_PROXY is just a filename, qualify it
+        X509_USER_PROXY=$(pwd)/$X509_USER_PROXY
     fi
     if [ -d ${X509_USER_PROXY} ]; then
-	VARS=$(modifyList "$VARS" "X509_USER_PROXY")
+        VARS=$(modifyList "$VARS" "X509_USER_PROXY")
     fi
 fi
 
@@ -292,7 +292,7 @@ fi
 libdirlist=${LD_LIBRARY_PATH}
 for subdir in /usr/lib /usr/lib64 /lib /lib64 /usr/lib/x86_64-linux-gnu; do
     if [ -d ${subdir} ]; then
-	libdirlist=$(appendPath "$libdirlist" "$subdir")
+        libdirlist=$(appendPath "$libdirlist" "$subdir")
     fi
 done
 
@@ -302,15 +302,15 @@ IFS=:
 for p in ${libdirlist}
 do
     if [ $_version -eq 2 ]; then
-	if [ -e ${p}/libOpenCL.so.1 ]; then
-	    tmp_ocl=${p}/libOpenCL.so.1
-	elif [ -e ${p}/libOpenCL.so ]; then
-	    tmp_ocl=${p}/libOpenCL.so
-	fi
+        if [ -e ${p}/libOpenCL.so.1 ]; then
+            tmp_ocl=${p}/libOpenCL.so.1
+        elif [ -e ${p}/libOpenCL.so ]; then
+            tmp_ocl=${p}/libOpenCL.so
+        fi
     fi
 
     if [ -e ${p}/libgfortran.so.3 ]; then
-	tmp_ftn=${p}/libgfortran.so.3
+        tmp_ftn=${p}/libgfortran.so.3
     fi
     # XXX should we also check "libgfortran.so"?
 done
@@ -330,7 +330,7 @@ if [ -d ${vpath} ]; then
     # XXX should this use ARCH to define the expected ICD name?
     if ( [ ! -e ${vpath}/amdocl64.icd ] && [ ! -e ${vpath}/intel64.icd ] )
     then
-	# didn't find any installable client drivers
+        # didn't find any installable client drivers
         found_icd=0
     fi
 
@@ -345,19 +345,19 @@ if ( [ -z ${tmp_ocl} ] || [ ${found_icd} -eq 0 ] ); then
     tmp_icd=${tmp_ocl}/etc/OpenCL/vendors
 
     if [ -d ${tmp_lib} ]; then
-	LD_LIBRARY_PATH=$(prependPath "$LD_LIBRARY_PATH" "$tmp_lib")
-	VARS=$(modifyList "$VARS" "LD_LIBRARY_PATH")
+        LD_LIBRARY_PATH=$(prependPath "$LD_LIBRARY_PATH" "$tmp_lib")
+        VARS=$(modifyList "$VARS" "LD_LIBRARY_PATH")
     fi
 
     if [ $_version -eq 2 ]; then
-	if [ "${OPENCL_VENDOR_PATH}" = "/etc/OpenCL/vendors" -a -d ${tmp_icl} ]
-	then
-	    # copy all ICD files into a single directory
+        if [ "${OPENCL_VENDOR_PATH}" = "/etc/OpenCL/vendors" -a -d ${tmp_icl} ]
+        then
+            # copy all ICD files into a single directory
             OPENCL_VENDOR_PATH=`mktemp -d 2>/dev/null || mktemp -d -t 'vendortmp'`
             cp -r /etc/OpenCL/vendors/* ${OPENCL_VENDOR_PATH}
             cp -r ${tmp_icd}/* ${OPENCL_VENDOR_PATH}
-	    VARS=$(modifyList "$VARS" "OPENCL_VENDOR_PATH")
-	fi
+            VARS=$(modifyList "$VARS" "OPENCL_VENDOR_PATH")
+        fi
     fi
 fi
 
@@ -365,8 +365,8 @@ fi
 if [ -z ${tmp_ftn} ]; then
     tmp_ftn="$SROOT/tools/gfortran"
     if [ -d ${tmp_ftn} ]; then
-	LD_LIBRARY_PATH=$(appendPath "$LD_LIBRARY_PATH" "$tmp_ftn")
-	VARS=$(modifyList "$VARS" "LD_LIBRARY_PATH")
+        LD_LIBRARY_PATH=$(appendPath "$LD_LIBRARY_PATH" "$tmp_ftn")
+        VARS=$(modifyList "$VARS" "LD_LIBRARY_PATH")
     fi
 fi
 
@@ -376,11 +376,11 @@ for name in ${VARS}
 do
   eval VALUE=\$$name
   case ${CUR_SHELL} in 
-	tcsh)
-	    echo 'setenv '$name' '\"$VALUE\"' ;' ;;
-	csh)
-	    echo 'setenv '$name' '\"$VALUE\"' ;' ;;
-	*)
-	    echo 'export '$name=\"$VALUE\"' ;' ;;
+        tcsh)
+            echo 'setenv '$name' '\"$VALUE\"' ;' ;;
+        csh)
+            echo 'setenv '$name' '\"$VALUE\"' ;' ;;
+        *)
+            echo 'export '$name=\"$VALUE\"' ;' ;;
     esac
 done
