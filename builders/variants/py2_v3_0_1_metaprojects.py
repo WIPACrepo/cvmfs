8a0101089d3d829cb9d01b019444aaa8f0b8ff18
David Schultz <davids24@gmail.com>
2018-06-28 17:07:40 -0500

    add new simulation release V06-00-03

--- builders/variants/py2_v3_0_1_metaprojects.py
+++ builders/variants/py2_v3_0_1_metaprojects.py
@@ -28,7 +28,7 @@ def build(src,dest,svn_up=None,svn_only=None,nightly=False,**build_kwargs):
         kwargs['svn_only'] = svn_only
 
     # releases
-#    tools['i3_metaproject']['simulation']['V06-00-02-RC2'](dir_name,**kwargs)
+    tools['i3_metaproject']['simulation']['V06-00-03'](dir_name,**kwargs)
 
     tools['i3_metaproject']['icerec']['V05-02-00'](dir_name,**kwargs)
 
