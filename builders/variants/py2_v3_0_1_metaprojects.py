3e1add1e795da449970b70725188ff3fe5b5b1c5
David Schultz <davids24@gmail.com>
2018-12-18 17:59:27 -0600

    add new sim RC

--- builders/variants/py2_v3_0_1_metaprojects.py
+++ builders/variants/py2_v3_0_1_metaprojects.py
@@ -32,6 +32,7 @@ def build(src,dest,svn_up=None,svn_only=None,nightly=False,**build_kwargs):
     tools['i3_metaproject']['simulation']['V06-01-00-RC1'](dir_name,**kwargs)
     tools['i3_metaproject']['simulation']['V06-01-00-RC2'](dir_name,**kwargs)
     tools['i3_metaproject']['simulation']['V06-01-00-RC3'](dir_name,**kwargs)
+    tools['i3_metaproject']['simulation']['V06-01-00-RC4'](dir_name,**kwargs)
 
     tools['i3_metaproject']['icerec']['V05-02-00'](dir_name,**kwargs)
 
