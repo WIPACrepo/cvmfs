1072eeae53f9ea05715f4507e26192eefbcc6fcf
David Schultz <davids24@gmail.com>
2019-01-07 11:37:19 -0600

    new simulation release and icerec candidate

--- builders/variants/py2_v3_0_1_metaprojects.py
+++ builders/variants/py2_v3_0_1_metaprojects.py
@@ -33,9 +33,11 @@ def build(src,dest,svn_up=None,svn_only=None,nightly=False,**build_kwargs):
     tools['i3_metaproject']['simulation']['V06-01-00-RC2'](dir_name,**kwargs)
     tools['i3_metaproject']['simulation']['V06-01-00-RC3'](dir_name,**kwargs)
     tools['i3_metaproject']['simulation']['V06-01-00-RC4'](dir_name,**kwargs)
+    tools['i3_metaproject']['simulation']['V06-01-00'](dir_name,**kwargs)
 
     tools['i3_metaproject']['icerec']['V05-02-00'](dir_name,**kwargs)
     tools['i3_metaproject']['icerec']['V05-02-01-RC1'](dir_name,**kwargs)
+    tools['i3_metaproject']['icerec']['V05-02-01-RC2'](dir_name,**kwargs)
 
     #if nightly:
         # trunks
