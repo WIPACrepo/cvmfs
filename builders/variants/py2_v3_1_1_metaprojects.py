2ec121dad1cf3d54cd6cb48430f2b2c8a52ed81a
David Schultz <davids24@gmail.com>
2020-02-07 14:36:43 -0600

    add new combo release

--- builders/variants/py2_v3_1_1_metaprojects.py
+++ builders/variants/py2_v3_1_1_metaprojects.py
@@ -33,8 +33,7 @@ def build(src,dest,svn_up=None,svn_only=None,nightly=False,**build_kwargs):
     tools['i3_metaproject']['icerec']['V05-02-05'](dir_name,**kwargs)
 
     tools['i3_metaproject']['combo']['V00-00-00'](dir_name,**kwargs)
-
-    tools['i3_metaproject']['combo']['V00-00-00-RC2'](dir_name,**kwargs)
+    tools['i3_metaproject']['combo']['V00-00-01'](dir_name,**kwargs)
 
     if nightly:
         # trunks
