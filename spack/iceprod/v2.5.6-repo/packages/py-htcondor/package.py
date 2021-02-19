1cd04ecd9fea2e52adb047505714b1d70e7d70fb
David Schultz <davids24@gmail.com>
2021-02-19 13:43:18 -0600

    fix a few repo/package issues for different iceprod versions

--- spack/iceprod/v2.5.6-repo/packages/py-htcondor/package.py
+++ spack/iceprod/v2.5.6-repo/packages/py-htcondor/package.py
@@ -25,16 +25,38 @@
 import os
 import inspect
 from spack import *
+from spack.util import web
 
 
 class PyHtcondor(PythonPackage):
     """The HTCondor python modules aim to expose a high-quality, Pythonic
     interface to the HTCondor client libraries."""
     homepage = "https://htcondor-python.readthedocs.io"
-    url = 'https://files.pythonhosted.org/packages/a5/fd/585d398ec544050b60449644aafef1b21e57feab5460b54313c9d5fb0c09/htcondor-8.7.9-cp36-cp36m-manylinux1_x86_64.whl'
+    url = 'https://files.pythonhosted.org/packages/1c/84/eee7908ece043e93a3866b79abbe9be8338229d81652c849906967de6cb0/htcondor-8.8.6-cp37-cp37m-manylinux1_x86_64.whl'
 
+    def url_for_version(self, version):
+        base_url = 'https://pypi.org/simple/htcondor/'
+        if self.spec.satisfies('^python@3.9:3.9.99'):
+            filename = 'htcondor-{}-cp39-cp39-manylinux1_x86_64.whl'.format(version)
+        elif self.spec.satisfies('^python@3.8:3.8.99'):
+            filename = 'htcondor-{}-cp38-cp38-manylinux1_x86_64.whl'.format(version)
+        elif self.spec.satisfies('^python@3.7:3.7.99'):
+            filename = 'htcondor-{}-cp37-cp37m-manylinux1_x86_64.whl'.format(version)
+        elif self.spec.satisfies('^python@3.6:3.6.99'):
+            filename = 'htcondor-{}-cp36-cp36m-manylinux1_x86_64.whl'.format(version)
+
+        response = web._urlopen(base_url)
+        readme = response.read().decode('utf-8')
+        name = None
+        for line in readme.split('\n'):
+            if filename in line:
+                return line.split('=',1)[-1].split('>',1)[0].strip('"\' ').split('#')[0]
+        else:
+            raise Exception('cannot find version')
+
+    version('8.9.11', sha256='fe568076cc3e4c20e7e77c03695c72d4082ccd14bb889e515ef7be9ac6bd3ad7', expand=False)
+    version('8.8.6', sha256='226534187ddf4dcd05ae3dcdd5007f3f41654bc65a874b4a2cc14573f6f66b8e', expand=False)
     version('8.7.9', sha256='17ae9767bc74b6fb007666eafcacb03c14b1c521b3e154f9ebb053e14376eaa2', expand=False)
-    version('8.9.11', sha256='fe568076cc3e4c20e7e77c03695c72d4082ccd14bb889e515ef7be9ac6bd3ad7', url='https://files.pythonhosted.org/packages/8c/c7/fad9fe2312df48f7647069ee7df0e1837373318c4172938ca6dad40d2ce0/htcondor-8.9.11-cp38-cp38-manylinux1_x86_64.whl', expand=False)
 
     depends_on('py-setuptools', type='build')
     depends_on('py-pip', type='build')
