8cb4807981419bd0cb606776afba788e0ad1a563
David Schultz <david.schultz@icecube.wisc.edu>
2022-03-15 21:20:42 -0500

    add dependencies for pyyaml

---
+++ spack/py3-v4.2-repo/packages/py-pyyaml/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyPyyaml(PythonPackage):
+    """PyYAML is a YAML parser and emitter for Python."""
+    homepage = "http://pyyaml.org/wiki/PyYAML"
+    url      = "http://pyyaml.org/download/pyyaml/PyYAML-3.11.tar.gz"
+
+    version('3.12', '4c129761b661d181ebf7ff4eb2d79950')
+    version('3.11', 'f50e08ef0fe55178479d3a618efe21db')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools-scm', type='build')
+    depends_on('py-wheel', type='build')
+    depends_on('py-packaging', type='build')
+    depends_on('py-pip', type='build')
