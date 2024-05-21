35856bf0ba45e1152c876567da9c45277f0718c6
David Schultz <david.schultz@icecube.wisc.edu>
2024-05-21 09:43:19 -0500

    make sure to load MAN from the host. fixes #67

--- icecube.opensciencegrid.org/py3-v4/setup/manpath.py
+++ icecube.opensciencegrid.org/py3-v4/setup/manpath.py
@@ -8,6 +8,9 @@ for subdir in ['man','share/man']:
     if os.path.isdir(path):
         ret.append(path)
 if 'MANPATH' in os.environ:
-    ret.extend(x for x in os.environ['MANPATH'].split(':') if x.strip())
+    ret.extend(x for x in os.environ['MANPATH'].split(':'))
 if ret:
+    if '' not in ret:
+        # add default manpath config after our config
+        ret.append('')
     print(':'.join(ret))
