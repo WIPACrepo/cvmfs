a301e702a269fb78155c5506b5582294d9b8e60e
Benedikt Riedel <benedikt.riedel@gmail.com>
2025-05-29 10:45:51 -0500

    changing things from arch to target

--- spack/build_v2.py
+++ spack/build_v2.py
@@ -398,7 +398,8 @@ spack:
       require: '"""
         if self.compiler_package:
             env_yaml += f'%{self.compiler_package} '
-        env_yaml += f"""arch={self.spack_arch["platform"]}-{self.spack_arch["platform_os"]}-{self.spack_arch["target"]}'"""
+        # env_yaml += f"""arch={self.spack_arch["platform"]}-{self.spack_arch["platform_os"]}-{self.spack_arch["target"]}'"""
+        env_yaml += f"""target={self.spack_arch["target"]}'"""
         if self.compiler_package:
             env_yaml += f"""
       compiler: [{self.compiler_package}]"""
